fde3133286725c8f8a5a7809729dbc91fd0c0950
Bfunk54 <ben_fein@icloud.com>
2022-11-15 23:16:12 -0500

    set up the index routes

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+// Require express
+const express = require("express");
+
+// Import our modular routers for /notes
+const notesRouter = require("./notes");
+
+const app = express();
+
+app.use("/notes", notesRouter);
+
+module.exports = app;
