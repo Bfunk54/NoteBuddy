ce4100705b5887751ce3096f33364b1720c4d726
Bfunk54 <ben_fein@icloud.com>
2022-11-20 17:28:55 -0500

    finally got the server to display the page

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(api);
+app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -20,7 +20,7 @@ app.get('/', (req, res) =>
 
 // GET Route for notes page
 app.get('/api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/notes.html'))
 );
 
 app.listen(PORT, () =>
