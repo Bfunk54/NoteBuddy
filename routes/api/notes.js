90bd40bcd495655577779c3bed5b24c6ad81ae92
Bfunk54 <ben_fein@icloud.com>
2022-11-20 18:16:02 -0500

    attempts to get javascript on the html

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,12 +1,12 @@
-const notes = require("express").Router();
+const note = require("express").Router();
 const {
   readFromFile,
   readAndAppend,
   writeToFile,
 } = require("./fsUtils");
 
-notes.get("/", (req, res) => {
+note.get("/", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
-module.exports = notes;
+module.exports = note;
