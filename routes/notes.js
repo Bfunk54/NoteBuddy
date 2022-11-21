40e482f3a15ca076a6f97eee024fe08c51faadf6
Bfunk54 <ben_fein@icloud.com>
2022-11-21 09:37:19 -0500

    added a post route for the notew

--- routes/notes.js
+++ routes/notes.js
@@ -9,4 +9,21 @@ note.get("/", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
+note.post("/", (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error("Could not add Note");
+  }
+});
+
 module.exports = note;
