e72960dc134fc6cba71f594bea2a0ad35141fe8f
Bfunk54 <ben_fein@icloud.com>
2022-11-21 10:26:57 -0500

    changed homepage to * rather than / as per requirements and had to switch around the order, but it now works!

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/index.html'))
-);
-
 // GET Route for notes page
 app.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, '/notes.html'))
+);
+
+// GET Route for homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/index.html'))
 );
 
 app.listen(PORT, () =>
