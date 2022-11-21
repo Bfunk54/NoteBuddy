6578a4fba2265d548461a15b6467183802ee626b
Bfunk54 <ben_fein@icloud.com>
2022-11-21 10:11:46 -0500

    now able to select old notes and view them

--- routes/notes.js
+++ routes/notes.js
@@ -13,17 +13,30 @@ note.post("/", (req, res) => {
   const { title, text } = req.body;
 
   if (title && text) {
-    const newNote = {
-      title,
-      text,
-      id: uuidv4(),
+    const note = {
+      title: title,
+      text: text,
+      id: uuidv4()
     };
 
-    readAndAppend(newNote, "./db/db.json");
+    readAndAppend(note, "./db/db.json");
     res.json(`Note added successfully 🚀`);
   } else {
     res.error("Could not add Note");
   }
+});
+
+note.delete("/:id", (req, res) => {
+  const note_id = req.params.id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== note_id);
+
+      writeToFile("./db/db.json", result);
+
+      res.json(`${note_id} has been deleted from the database`);
+    });
 });
 
 module.exports = note;
