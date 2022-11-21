596dcbc3311b0446b9ef9071a2a296ac1145c7bb
Bfunk54 <ben_fein@icloud.com>
2022-11-21 09:50:15 -0500

    finally got notes saved in the data base to show up!

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('./routes/api', api);
+app.use('/api', api);
 
 
 app.use(express.static('public'));
--- public/assets/js/notes.js
+++ public/assets/js/notes.js
@@ -26,7 +26,7 @@ const show = (elem) => {
   let activeNote = {};
   
   const getNotes = () =>
-    fetch('/notes', {
+    fetch('/api/notes', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -34,7 +34,7 @@ const show = (elem) => {
     });
   
   const saveNote = (note) =>
-    fetch('/notes', {
+    fetch('/api/notes', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -43,7 +43,7 @@ const show = (elem) => {
     });
   
   const deleteNote = (id) =>
-    fetch(`/notes/${id}`, {
+    fetch(`/api/notes/${id}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
